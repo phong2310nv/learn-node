06231a37aedaaa2267dfe8988873f2e770840f27
phong2310nv <phong2310nv@gmail.com>
2025-03-19 18:49:49 +0700

    fix: improve linting

--- nodejs-express-mongodb-bootcamp/natours/dev/server.js
+++ nodejs-express-mongodb-bootcamp/natours/dev/server.js
@@ -1,7 +1,6 @@
 const dotenv = require('dotenv');
-const app = require('./app');
-
 dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
