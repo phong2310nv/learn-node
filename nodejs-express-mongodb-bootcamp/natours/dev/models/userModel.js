4a1fe5867d87e1b08d9ebf65b582b96538a35641
phongvan2310 <123443380+phongvan2310@users.noreply.github.com>
2025-04-14 01:28:26 +0700

    feat: add user signup

---
+++ nodejs-express-mongodb-bootcamp/natours/dev/models/userModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please tell us your name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide your email'],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, 'Please provide a valid email'],
+    },
+    photo: String,
+    password: {
+      type: String,
+      required: [true, 'Please provide a password'],
+      minlength: 8,
+      select: false,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Please confirm your password'],
+      validate: {
+        // This only works on CREATE and SAVE!!!
+        validator(el) {
+          return el === this.password;
+        },
+        message: 'Passwords are not the same!',
+      },
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre('save', async function (next) {
+  // Check if the password is modified before hashing
+  // If the password is not modified, skip hashing
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(12); // 12 is the number of rounds
+  this.password = await bcrypt.hash(this.password, salt);
+  this.passwordConfirm = undefined; // Remove passwordConfirm from the document
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
